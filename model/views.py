d8f70dfc948d2a8d5ec7b7994a2f9e925cb296f2
pankaj singh chamyal <pankajsinghchamyal569@gmail.com>
2020-10-25 06:29:52 +0530

    model predict properly

--- model/views.py
+++ model/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import joblib
+import numpy as np
 
 # Create your views here.
 def home(request):
@@ -8,4 +9,8 @@ def home(request):
 
 def predict(request):
     regressor = joblib.load("gbregressor.pkl")
+    l =  [3,1,1180,5650,1,0,0,3,7,1180,0,1955,0,98178,47.5112,-122.257,1340,5650]
+    l = np.array(l)
+    l = np.reshape(l,(1,l.shape[0]))
+    print("result:    " ,regressor.predict(l))
     return HttpResponse('<h1> Result </h1>')
