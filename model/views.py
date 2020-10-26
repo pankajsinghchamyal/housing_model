b4a3c0ac291e5eaf6f9976b4db2fe9f688118f8a
pankaj singh chamyal <pankajsinghchamyal569@gmail.com>
2020-10-26 23:19:54 +0530

    prediction start

--- model/views.py
+++ model/views.py
@@ -10,30 +10,37 @@ def home(request):
 def predict(request):
     regressor = joblib.load("gbregressor.pkl")
     l = []
+    feature = []
+    def extract_value(key):
+        l.append(float(request.POST[key]))
+        feature.append(request.POST[key])
+    cols = ['bedrooms','bathrooms','sqft_living','sqft_lot','floors','waterfront','view','condition','grade','sqft_above','sqft_basement','yr_built','yr_renovated','zipcode','lat','long','sqft_living15','sqft_lot15']
     if(request.method == 'POST'):
-        l.append(request.POST['bedrooms'])
-        l.append(request.POST['bathrooms'])
-        l.append(request.POST['sqft_living'])
-        l.append(request.POST['sqft_lot'])
-        l.append(request.POST['floors'])
-        l.append(request.POST['waterfront'])
-        l.append(request.POST['view'])
-        l.append(request.POST['condition'])
-        l.append(request.POST['grade'])
-        l.append(request.POST['sqft_above'])
-        l.append(request.POST['sqft_basement'])
-        l.append(request.POST['yr_built'])
-        l.append(request.POST['yr_renovated'])
-        l.append(request.POST['zipcode'])
-        l.append(request.POST['lat'])
-        l.append(request.POST['long'])
-        l.append(request.POST['sqft_living15'])
-        l.append(request.POST['sqft_lot15'])
+        for key in cols:
+            extract_value(key)
+        # l.append(float(request.POST['bedrooms']))
+        # l.append(float(request.POST['bathrooms']))
+        # l.append(float(request.POST['sqft_living']))
+        # l.append(float(request.POST['sqft_lot']))
+        # l.append(float(request.POST['floors']))
+        # l.append(float(request.POST['waterfront']))
+        # l.append(float(request.POST['view']))
+        # l.append(float(request.POST['condition']))
+        # l.append(float(request.POST['grade']))
+        # l.append(float(request.POST['sqft_above']))
+        # l.append(float(request.POST['sqft_basement']))
+        # l.append(float(request.POST['yr_built']))
+        # l.append(float(request.POST['yr_renovated']))
+        # l.append(float(request.POST['zipcode']))
+        # l.append(float(request.POST['lat']))
+        # l.append(float(request.POST['long']))
+        # l.append(float(request.POST['sqft_living15']))
+        # l.append(float(request.POST['sqft_lot15']))
         
 
     # l =  [3,1,1180,5650,1,0,0,3,7,1180,0,1955,0,98178,47.5112,-122.257,1340,5650]
     l = np.array(l)
     l = np.reshape(l,(1,l.shape[0]))
-    print("result:    " ,regressor.predict(l))
-    res = regressor.predict(l)
-    return HttpResponse(str(res))
+    
+    res =  regressor.predict(l)
+    return render(request,"result.html",{'feature':feature,"res":res})
